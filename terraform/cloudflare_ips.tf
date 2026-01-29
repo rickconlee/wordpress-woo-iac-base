a3023d5bc4005f0acc25e3f4a8f23fefbea094f8
Rick Conlee <rick.conlee@gmail.com>
2026-01-29 07:31:58 -0500

    Added some known Cloudflare subnets

---
+++ terraform/cloudflare_ips.tf
@@ -0,0 +1,17 @@
+# These are the known public subnets that Cloudflare uses. 
+# We need these so we can tightly control ingress and egress from the infrastructure we are going to deploy in AWS
+
+locals {
+  cloudflare_ipv4 = [
+    "173.245.48.0/20",
+    "103.21.244.0/22",
+    "103.22.200.0/22",
+    "103.31.4.0/22",
+    "141.101.64.0/18",
+    "108.162.192.0/18",
+    "190.93.240.0/20",
+    "188.114.96.0/20",
+    "197.234.240.0/22",
+    "198.41.128.0/17"
+  ]
+}
