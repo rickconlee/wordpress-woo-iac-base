58a41ccf6a97b2131071ada7e7dd540bf389dd47
Rick Conlee <rick.conlee@gmail.com>
2026-01-29 15:08:01 -0500

    Formatting

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,18 +1,21 @@
 terraform {
   required_version = ">= 1.6.0"
 
+# Included providers
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
-    
     cloudflare = {
       source  = "cloudflare/cloudflare"
       version = "~> 4.0"
     }
   }
 }
+
+# Provider specific configuration items
 
 provider "aws" {
   region = "us-east-2"
