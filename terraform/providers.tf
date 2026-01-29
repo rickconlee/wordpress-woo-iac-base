4f38cf7f7cc0de1c7f14fc1a16a351f30a43470d
Rick Conlee <rick.conlee@gmail.com>
2026-01-29 15:06:46 -0500

    Added cloudflare provider

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -6,6 +6,11 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 4.0"
+    }
   }
 }
 
