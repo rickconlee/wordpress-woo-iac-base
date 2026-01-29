2d61d2085387532e00c23a2e402bc39bdfdea050
Rick Conlee <rick.conlee@gmail.com>
2026-01-29 07:34:45 -0500

    Initial put of the Terraform code that we will be using to create the infrastructure, hacked together from some templates I had laying around.

---
+++ terraform/main.tf
@@ -0,0 +1,209 @@
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+##################
+# Networking
+##################
+
+resource "aws_vpc" "demo" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "a" {
+  vpc_id            = aws_vpc.demo.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-2a"
+}
+
+resource "aws_subnet" "b" {
+  vpc_id            = aws_vpc.demo.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-2b"
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.demo.id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.demo.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.a.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "b" {
+  subnet_id      = aws_subnet.b.id
+  route_table_id = aws_route_table.public.id
+}
+
+##################
+# Security Groups
+##################
+
+resource "aws_security_group" "efs" {
+  vpc_id = aws_vpc.demo.id
+
+  ingress {
+    from_port       = 2049
+    to_port         = 2049
+    protocol        = "tcp"
+    security_groups = [aws_security_group.web.id]
+  }
+
+  egress {
+    from_port       = 2049
+    to_port         = 2049
+    protocol        = "tcp"
+    security_groups = [aws_security_group.web.id]
+  }
+}
+
+resource "aws_security_group" "web" {
+  vpc_id = aws_vpc.demo.id
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = local.cloudflare_ipv4
+  }
+
+  ingress {
+    from_port       = 2049
+    to_port         = 2049
+    protocol        = "tcp"
+    security_groups = [aws_security_group.efs.id]
+  }
+
+  egress {
+    from_port       = 2049
+    to_port         = 2049
+    protocol        = "tcp"
+    security_groups = [aws_security_group.efs.id]
+  }
+}
+
+##################
+# EFS
+##################
+
+resource "aws_efs_file_system" "wp" {
+  throughput_mode = "bursting"
+}
+
+resource "aws_efs_mount_target" "a" {
+  file_system_id  = aws_efs_file_system.wp.id
+  subnet_id       = aws_subnet.a.id
+  security_groups = [aws_security_group.efs.id]
+}
+
+resource "aws_efs_mount_target" "b" {
+  file_system_id  = aws_efs_file_system.wp.id
+  subnet_id       = aws_subnet.b.id
+  security_groups = [aws_security_group.efs.id]
+}
+
+##################
+# RDS
+##################
+
+resource "aws_db_subnet_group" "db" {
+  subnet_ids = [aws_subnet.a.id, aws_subnet.b.id]
+}
+
+resource "aws_db_instance" "mysql" {
+  engine                 = "mysql"
+  instance_class         = "db.t3.micro"
+  allocated_storage      = 20
+  db_name                = "wordpress"
+  username               = "wpadmin"
+  password               = "changeme123!"
+  skip_final_snapshot    = true
+  db_subnet_group_name   = aws_db_subnet_group.db.name
+  vpc_security_group_ids = [aws_security_group.web.id]
+}
+
+##################
+# NLB
+##################
+
+resource "aws_lb" "nlb" {
+  name               = "lolzify-nlb"
+  load_balancer_type = "network"
+  subnets            = [aws_subnet.a.id, aws_subnet.b.id]
+}
+
+resource "aws_lb_target_group" "tg" {
+  port     = 443
+  protocol = "TCP"
+  vpc_id  = aws_vpc.demo.id
+
+  health_check {
+    protocol = "TCP"
+  }
+}
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_lb.nlb.arn
+  port              = 443
+  protocol          = "TCP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+}
+
+##################
+# EC2 / ASG
+##################
+
+resource "aws_launch_template" "wp" {
+  image_id      = "ami-ANSIBLE-BUILT"
+  instance_type = "t2.micro"
+
+  network_interfaces {
+    associate_public_ip_address = true
+    security_groups             = [aws_security_group.web.id]
+  }
+}
+
+resource "aws_autoscaling_group" "wp" {
+  desired_capacity = 2
+  min_size         = 2
+  max_size         = 2
+  vpc_zone_identifier = [
+    aws_subnet.a.id,
+    aws_subnet.b.id
+  ]
+
+  launch_template {
+    id      = aws_launch_template.wp.id
+    version = "$Latest"
+  }
+}
+
+resource "aws_autoscaling_attachment" "asg" {
+  autoscaling_group_name = aws_autoscaling_group.wp.name
+  lb_target_group_arn    = aws_lb_target_group.tg.arn
+}
